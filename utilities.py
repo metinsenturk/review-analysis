198ff5927ee76897706bc7088fadee9181f23fc3
Metin Senturk <metinsenturk@me.com>
2018-10-12 00:51:25 -0400

    adding fb and yelp

--- utilities.py
+++ utilities.py
@@ -2,9 +2,6 @@ import json
 import os
 
 def get_credidentials():  
-    json_data = open("{}/{}".format(os.getcwd(), "sanalysis/parameters.json")).read() 
+    json_data = open("{}/{}".format(os.getcwd(), "parameters.json")).read() 
     data = json.loads(json_data)
     return(data['data'])
-
-if __name__ == '__main__':
-    get_credidentials()
