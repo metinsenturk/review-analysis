f60b69a42e6e5f3acfdb79995f921386678bd221
Metin Senturk <metinsenturk@me.com>
2019-01-12 23:23:58 -0500

    setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='reviewanalysis',
+   version='0.1.0',
+   description='Sentiment and topic modeling implementation on reviews.',
+   author='Metin S & Anika Patel',
+   author_email='metinsenturk@me.com',
+   packages=['reviewanalysis'],  #same as name
+   install_requires=[]
+)
