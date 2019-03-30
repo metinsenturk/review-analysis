13f17d02e5c2ff01c5108c60144df6a86c2f4e4b
Metin Senturk <metinsenturk@me.com>
2019-03-30 19:34:49 -0400

    server commands added.

---
+++ src/server_commands.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# get model directory -- method 1
+# scp -r msenturk@dsl.saintpeters.edu:/home/msenturk/review-analysis/model/ ../model_server/
+rsync -av --exclude mallet/ msenturk@dsl.saintpeters.edu:/home/msenturk/review-analysis/model/ ../model_server/
+
+# get topic results file
+scp msenturk@dsl.saintpeters.edu:/home/msenturk/review-analysis/data/processed/yp_competitors_rws_0001_0050_topics.csv ../data/processed/yp_competitors_rws_0001_0050_topics_server.csv
+
+scp msenturk@dsl.saintpeters.edu:/home/msenturk/review-analysis/data/processed/hi_rws_0001_0256_topics.csv ../data/processed/hi_rws_0001_0256_topics_server.csv
