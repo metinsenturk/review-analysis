562fc772937ef765f5b4448e51f673f6bded2100
Metin Senturk <metinsenturk@me.com>
2019-04-07 17:53:08 -0400

    package requirements updated with SpaCy.

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(
    description='Sentiment and topic modeling implementation on reviews.',
    long_description=readme(),
    url='https://github.com/metinsenturk/review-analysis',
-   license='MIT'
+   license='MIT',
    author='Metin Senturk',
    author_email='metinsenturk@me.com',
    packages=['reviewanalysis'],  #same as name
-   dependency_links=['https://github.com/mimno/Mallet/archive/v2.0.8RC3.zip']
+   dependency_links=['https://github.com/mimno/Mallet/archive/v2.0.8RC3.zip'],
    install_requires=[]
 )
