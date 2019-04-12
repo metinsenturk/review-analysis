82f5476d01345eda9c967000ba16beabfda48bd2
Metin Senturk <metinsenturk@me.com>
2019-04-11 23:35:27 -0400

    setup version updated.

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ def readme():
         return f.read()
 
 setup(
-    name='reviewanalysis',
-   version='0.1.0',
+   name='review_analysis',
+   version='0.0.9',
    description='Sentiment and topic modeling implementation on reviews.',
    long_description=readme(),
    url='https://github.com/metinsenturk/review-analysis',
    license='MIT',
    author='Metin Senturk',
    author_email='metinsenturk@me.com',
-   packages=['reviewanalysis'],  #same as name
+   packages=['review_analysis'],  #same as name
    dependency_links=['https://github.com/mimno/Mallet/archive/v2.0.8RC3.zip'],
    install_requires=[]
 )
